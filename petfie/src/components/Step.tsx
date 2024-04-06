d7d6bca6aea065b386048d6df7b6ee7adb36c46f
JEONGMIN <gyflsakfn@naver.com>
2024-04-07 01:15:58 +0900

    [Fix]: conflict merge fix

--- petfie/src/components/Step.tsx
+++ petfie/src/components/Step.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { Button } from "@radix-ui/themes";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { StepDone } from "@/components/StepDone";
+import { CardPreview } from "@/components/CardPreview";
 import InfoForm from "@/components/InfoForm";
 import { Info } from "@/components/InfoForm.types";
 
@@ -34,6 +35,10 @@ export default function Step() {
     console.log("info", info);
   }, [info]);
 
+  // 카드 미리보기 state
+  const [imgUrl, setImgUrl] = useState("/asset/animal2.jpg");
+  const [frameUrl, setFrameUrl] = useState("/asset/frame6.png");
+
   return (
     <>
       {/* Main layout */}
@@ -42,11 +47,36 @@ export default function Step() {
         {step === 1 && <InfoForm info={info} changeInfo={setInfo} />}
         {step === 2 && <StepDone />}
       </div>
+      {/* Card Preview */}
+      <div
+        className={`w-full flex justify-center items-center min-h-[310px] rounded-lg ${
+          step !== 2 && "bg-black"
+        }`}
+      >
+        {
+          <div
+            className={
+              step === 2 ? "w-[316px] h-[454px]" : "w-[198px] h-[284px]"
+            }
+          >
+            <CardPreview
+              step={step}
+              imgUrl={imgUrl}
+              frameUrl={frameUrl}
+              info={info}
+            />
+          </div>
+        }
+      </div>
+
       {/* Footer layout */}
       {step === 1 && <Button onClick={decreaseStep}>이전</Button>}
       {step !== 2 && <Button onClick={increaseStep}>다음</Button>}
       {step === 2 && (
-        <Button onClick={goToFirstStep}>다른 카드 만들러 가기</Button>
+        <>
+          <StepDone />
+          <Button onClick={goToFirstStep}>다른 카드 만들러 가기</Button>
+        </>
       )}
     </>
   );
