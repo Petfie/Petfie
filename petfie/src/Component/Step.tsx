dfadcd8075b4ca8bd78ec3fab4dd3ccf35f6e4fb
Mike <mike.kangaroo.world@gmail.com>
2024-04-06 14:11:00 +0900

    [feat] create step component

---
+++ petfie/src/Component/Step.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@radix-ui/themes";
+import { useState } from "react";
+
+export default function Step0() {
+  // step state
+  const [step, setStep] = useState(0);
+
+  const increaseStep = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+  const decreaseStep = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  const goToFirstStep = () => {
+    setStep(0);
+  };
+
+  return (
+    <>
+      {/* Main layout */}
+      <div>
+        {step === 0 && <div>step 0 템플릿 선택 / 사진 업로드</div>}
+        {step === 1 && <div>step 1 커스텀 하기 / 완성하기</div>}
+        {step === 2 && <div>step 2 저장하기 / URL 공유하기</div>}
+      </div>
+      {/* Footer layout */}
+      {step === 1 && <Button onClick={decreaseStep}>이전</Button>}
+      {step !== 2 && <Button onClick={increaseStep}>다음</Button>}
+      {step === 2 && (
+        <Button onClick={goToFirstStep}>다른 카드 만들러 가기</Button>
+      )}
+    </>
+  );
+}
