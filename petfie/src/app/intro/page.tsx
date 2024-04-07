e0f57d64a1d657222483f598ffe46af4ea72851d
youngjun.choi <dlfgudwns12@gmail.com>
2024-04-07 13:39:04 +0900

    [design] 인트로 페이지 퍼블리싱 완료, layout.tsx에서 radix style 제거, global css 추가

--- petfie/src/app/intro/page.tsx
+++ petfie/src/app/intro/page.tsx
@@ -1,28 +1,35 @@
-import { Button } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
+import "./page.css";
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 
 export default function intro() {
   return (
     <>
-      <h1>Petfie</h1>
-      <div>
-        <h2>반려동물로 카드를 만들어보세요</h2>
-        <h3>나만의 유니크한 반려동물 카드 친구에게 공유하며 자랑해요</h3>
-      </div>
-      <ul className="grid-cols-2">
-        <li>
+      <div className="title-cont">
+        <h1>
           <Image
-            src="/asset/완성카드.png"
-            alt="미리보기 이미지"
-            width={200}
-            height={200}
+          src="/asset/logo.png"
+          alt="Petfie 로고 이미지"
+          width={110}
+          height={36}
           />
-        </li>
-      </ul>
-      <Link href={"/main"}>
-        <Button>시작하기</Button>
-      </Link>
+        </h1>
+        <QuestionMarkCircledIcon width={32} height={32}/>
+      </div>
+      <h2 className="comment">반려동물과의 추억을<br/>나만의 카드를 담아보세요</h2>
+      <Image
+        className="preview-img"
+        src="/asset/완성카드.png"
+        alt="미리보기 이미지"
+        width={200}
+        height={200}
+      />
+      <div className="button-cont">
+        <Link href={"/main"}>
+          <button className="start-button button-next">시작하기</button>
+        </Link>
+      </div>
     </>
   );
 }
