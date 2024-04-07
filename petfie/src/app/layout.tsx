9240b2e4eabc6d78a515833f9f57c76921a16a7a
Zalgritte <Zalgritte@gmail.com>
2024-04-07 16:25:20 +0900

    opengraph작업

--- petfie/src/app/layout.tsx
+++ petfie/src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { Theme } from "@radix-ui/themes";
 
-export const metadata: Metadata = {
+
+export const metadata = {
   title: "Petfie",
   description: "Build your pet's profile cards with Petfie.",
 };
@@ -14,7 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        {/* 기본 메타 태그 */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/asset/opengraph-image.jpg" />
+        {/* 내부에 업로드한 OG 이미지 URL을 사용 */}
+      </head>
+      <body>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
